f8c3f56a71144a486654205d6dbbec9209b8528b
COGMLS <mlsmatheus@hotmail.com>
2022-08-22 19:51:03 -0300

    Add LedBlink.hpp and LedBlink.cpp

---
+++ Libs/LED/LedBlink/LedBlink.cpp
@@ -0,0 +1,240 @@
+#include "LedBlink.hpp"
+
+// private functions:
+
+// Make the change ON or OFF
+void LedBlink::changeLedStatus()
+{
+    if(this->ledStatus)
+    {
+        this->ledStatus = false;
+    }
+    else
+    {
+        this->ledStatus = true;
+    }
+
+    digitalWrite(this->ledPin, this->ledStatus);
+}
+
+void LedBlink::ledBlink()
+{
+    this->changeLedStatus();
+    delay(this->ledDelay);
+    this->changeLedStatus();
+}
+
+// Contructors and destructor:
+LedBlink::LedBlink(int ledPin)
+{
+    this->ledPin = ledPin;
+    this->ledStatus = false;
+    this->ledDelay = 500;  // 500 ms
+    this->blinkStyle = ledBlinkStyle::SIMPLE_BLINK;
+    this->ledMultiBlinkInterval = 100;  // 100 ms
+    digitalWrite(this->ledPin, this->ledStatus);
+}
+LedBlink::LedBlink(int ledPin, int ledDelay)
+{
+    this->ledPin = ledPin;
+    this->ledStatus = false;
+    this->ledDelay = ledDelay;
+    this->blinkStyle = ledBlinkStyle::SIMPLE_BLINK;
+    this->ledMultiBlinkInterval = 100;  // 100 ms
+    digitalWrite(this->ledPin, this->ledStatus);
+}
+LedBlink::LedBlink(int ledPin, enum ledBlinkStyle blinkStyle)
+{
+    this->ledPin = ledPin;
+    this->ledStatus = false;
+    this->ledDelay = 500;  // 500 ms
+    this->blinkStyle = blinkStyle;
+    this->ledMultiBlinkInterval = 100;  // 100 ms
+    digitalWrite(this->ledPin, this->ledStatus);
+}
+LedBlink::LedBlink(int ledPin, int ledDelay, enum ledBlinkStyle blinkStyle)
+{
+    this->ledPin = ledPin;
+    this->ledStatus = false;
+    this->ledDelay = ledDelay;
+    this->blinkStyle = blinkStyle;
+    this->ledMultiBlinkInterval = 100;  // 100 ms
+    digitalWrite(this->ledPin, this->ledStatus);
+}
+LedBlink::LedBlink(int ledPin, enum ledBlinkStyle blinkStyle, int ledMultiBlinkInterval)
+{
+    this->ledPin = ledPin;
+    this->ledStatus = false;
+    this->ledDelay = 500;  // 500 ms
+    this->blinkStyle = blinkStyle;
+    this->ledMultiBlinkInterval = ledMultiBlinkInterval;
+    digitalWrite(this->ledPin, this->ledStatus);
+}
+LedBlink::LedBlink(int ledPin, int ledDelay, enum ledBlinkStyle blinkStyle, int ledMultiBlinkInterval)
+{
+    this->ledPin = ledPin;
+    this->ledStatus = false;
+    this->ledDelay = ledDelay;
+    this->blinkStyle = blinkStyle;
+    this->ledMultiBlinkInterval = ledMultiBlinkInterval;
+    digitalWrite(this->ledPin, this->ledStatus);
+}
+LedBlink::~LedBlink()
+{
+
+}
+
+// Getters:
+
+// Vefiry if the LED is blinking
+bool LedBlink::isBlinking()
+{
+    if(this->ledBlinking && this->ledStatus)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int LedBlink::getDelayBlinking()
+{
+    return this->ledDelay;
+}
+
+int LedBlink::getlMultiBlinkInterval()
+{
+    return this->ledMultiBlinkInterval;
+}
+
+enum ledBlinkStyle LedBlink::getBlinkStyle()
+{
+    return this->blinkStyle;
+}
+
+// Setters:
+
+void LedBlink::setDelayBliking(int ledDelay)
+{
+    this->ledDelay = ledDelay;
+}
+void LedBlink::setMultiBlinkInterval(int ledMultiBlinkInterval)
+{
+    this->ledMultiBlinkInterval = ledMultiBlinkInterval;
+}
+void LedBlink::setBlinkStyle(enum ledBlinkStyle blinkStyle)
+{
+    this->blinkStyle = blinkStyle;
+}
+void LedBlink::setBlinkStyle(enum ledBlinkStyle blinkStyle, int ledDelay)
+{
+    this->blinkStyle = blinkStyle;
+    this->ledDelay = ledDelay;
+}
+
+// Other controls:
+
+// Make the LED blink, if the blinkStyle was setted, otherwise will make the LED ON
+void LedBlink::blinkNow()
+{
+    if(this->blinkStyle == ledBlinkStyle::NO_BLINK)
+    {
+        // Set LED ON
+        this->setOn();
+    }
+    else if(this->blinkStyle == ledBlinkStyle::SIMPLE_BLINK)
+    {
+        this->ledBlinking = true;
+
+        // Simple blink
+        this->ledBlink();
+
+        this->ledBlinking = false;
+    }
+    else if(this->blinkStyle == ledBlinkStyle::DUAL_BLINK)
+    {
+        this->ledBlinking = true;
+
+        // Dual blink
+        this->ledBlink();
+        delay(this->ledMultiBlinkInterval);
+        this->ledBlink();
+
+        this->ledBlinking = false;
+    }
+    else if(this->blinkStyle == ledBlinkStyle::TRIPLE_BLINK)
+    {
+        this->ledBlinking = true;
+
+        // Triple blink
+        this->ledBlink();
+        delay(this->ledMultiBlinkInterval);
+        this->ledBlink();
+        delay(this->ledMultiBlinkInterval);
+        this->ledBlink();
+
+        this->ledBlinking = false;
+    }
+    else if(this->blinkStyle == ledBlinkStyle::SIMPLE_DUAL_BLINK)
+    {
+        this->ledBlinking = true;
+
+        // Simple blink
+        this->ledBlink();
+
+        delay(this->ledDelay);
+        
+        // Dual blink
+        this->ledBlink();
+        delay(this->ledMultiBlinkInterval);
+        this->ledBlink();
+
+        this->ledBlinking = false;
+    }
+    else if(this->blinkStyle == ledBlinkStyle::SIMPLE_TRIPLE_BLINK)
+    {
+        this->ledBlinking = true;
+
+        // Simple blink
+        this->ledBlink();
+
+        delay(this->ledDelay);
+        
+        // Triple blink
+        this->ledBlink();
+        delay(this->ledMultiBlinkInterval);
+        this->ledBlink();
+        delay(this->ledMultiBlinkInterval);
+        this->ledBlink();
+
+        this->ledBlinking = false;
+    }
+    else if(this->blinkStyle == ledBlinkStyle::DUAL_TRIPLE_BLINK)
+    {
+        this->ledBlinking = true;
+
+        // Dual blink
+        this->ledBlink();
+        delay(this->ledMultiBlinkInterval);
+        this->ledBlink();
+
+        delay(this->ledDelay);
+        
+        // Triple blink
+        this->ledBlink();
+        delay(this->ledMultiBlinkInterval);
+        this->ledBlink();
+        delay(this->ledMultiBlinkInterval);
+        this->ledBlink();
+
+        this->ledBlinking = false;
+    }
+}
+
+// Make the LED OFF, if blinking, but also will set to OFF is blinkStyle is NO_BLINK
+void LedBlink::stopBlik()
+{
+    this->setOff();
+}
---
+++ Libs/LED/LedBlink/LedBlink.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#ifndef ARDUINO_LED_BLINK_HPP
+#define ARDUINO_LED_BLINK_HPP
+
+#include "../LedControl/LedControl.hpp"
+
+enum ledBlinkStyle
+{
+    NO_BLINK,               // No LED blink
+    SIMPLE_BLINK,           // Make a simple LED blink
+    DUAL_BLINK,             // Make a dual LED blink
+    TRIPLE_BLINK,           // Make a triple LED blink
+    SIMPLE_DUAL_BLINK,      // Make a simple LED blink and a next dual blink
+    SIMPLE_TRIPLE_BLINK,    // Make a simple LED blink and a next triple blink
+    DUAL_TRIPLE_BLINK       // Make a dual LED blink and a triple blink
+};
+
+class LedBlink : public LedControl
+{
+    private:
+        bool ledBlinking;                   // Define if the LED is blinking or not
+        int ledDelay;                       // Define the LED blink
+        int ledMultiBlinkInterval;          // Define the LED multi blinking style interval
+        enum ledBlinkStyle blinkStyle;      // Define the LED blink style
+
+        // private functions:
+        void changeLedStatus();             // Make the change ON or OFF
+        void ledBlink();                    // Switch ON and OFF the LED with the delay
+    public:
+        // Contructors and destructor:
+        LedBlink(int ledPin);
+        LedBlink(int ledPin, int ledDelay);
+        LedBlink(int ledPin, enum ledBlinkStyle blinkStyle);
+        LedBlink(int ledPin, int ledDelay, enum ledBlinkStyle blinkStyle);
+        LedBlink(int ledPin, enum ledBlinkStyle blinkStyle, int ledMultiBlinkInterval);
+        LedBlink(int ledPin, int ledDelay, enum ledBlinkStyle blinkStyle, int ledMultiBlinkInterval);
+        ~LedBlink();
+
+        // Getters:
+        bool isBlinking();                  // Vefiry if the LED is blinking
+        int getDelayBlinking();
+        int getlMultiBlinkInterval();
+        enum ledBlinkStyle getBlinkStyle();
+
+        // Setters:
+        void setDelayBliking(int ledDelay);
+        void setMultiBlinkInterval(int ledMultiBlinkInterval);
+        void setBlinkStyle(enum ledBlinkStyle blinkStyle);
+        void setBlinkStyle(enum ledBlinkStyle blinkStyle, int ledDelay);
+
+        // Other controls:
+        void blinkNow();        // Make the LED blink, if the blinkStyle was setted, otherwise will make the LED ON
+        void stopBlik();        // Make the LED OFF, if blinking, but also will set to OFF is blinkStyle is NO_BLINK
+};
+
+#endif // ARDUINO_LED_BLINK_HPP
