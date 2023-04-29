498e4ec95c3f0efba15e4bc403a638b7ca4cb433
COGMLS <mlsmatheus@hotmail.com>
2023-04-29 13:19:25 -0300

    Add type.hpp to MemoryOptimizeLib Add ByteNibbleTable.hpp Add BinaryTable.hpp

---
+++ Libs/MemoryOptimize/BinaryTypes/BinaryTable.hpp
@@ -0,0 +1,53 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
+
+#pragma once
+
+#ifndef MEMORY_OPTIMIZE_BIN_TABLE_HPP
+#define MEMORY_OPTIMIZE_BIN_TABLE_HPP
+
+namespace memopt
+{
+    typedef struct byte_t
+    {
+        unsigned char data;
+
+        unsigned char getHighestNibble()
+        {
+            return ((data & 0b11110000) >> 0b100);
+        }
+
+        unsigned char getLowestNibble()
+        {
+            return (data & 0b00001111);
+        }
+
+        void setHighestNibble(unsigned char nibble)
+        {
+            nibble = (nibble & 0b00001111);		// Make sure that only the values on range 0-15 are available
+
+            nibble = (nibble << 0b100);
+
+            data = (data & 0b00001111);	// Clean the highest nibble
+            data = (data | nibble);
+        }
+
+        void setLowestNibble(unsigned char nibble)
+        {
+            nibble = (nibble & 0b00001111);     // Make sure that only the values on range 0-15 are available
+            data = (data & 0b11110000);	        // Clean the lowest nibble
+            data = (data | nibble);
+        }
+    };
+} // namespace memopt
+
+
+#endif // !MEMORY_OPTIMIZE_BIN_TABLE_HPP
