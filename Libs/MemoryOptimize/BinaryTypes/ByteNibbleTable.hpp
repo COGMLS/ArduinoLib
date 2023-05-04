a39a540e853266d1a892910eb029d7108e1d319c
COGMLS <mlsmatheus@hotmail.com>
2023-05-04 19:47:21 -0300

    Add documentation to nibble_t

--- Libs/MemoryOptimize/BinaryTypes/ByteNibbleTable.hpp
+++ Libs/MemoryOptimize/BinaryTypes/ByteNibbleTable.hpp
@@ -1,12 +1,12 @@
-/**
+/** Byte Nibble Table Library
+ * ------------------------------------
+ * @brief Provide an nibble level data control to store two diferent datas in one byte
  * 
+ * @author Matheus L. Silvati
  * 
+ * @version 1.2.0
  * 
- * 
- * 
- * 
- * 
- * 
+ * @note This library don't define the byte type, but uses the unsigned char as a byte
 */
 
 #pragma once
@@ -16,20 +16,26 @@
 
 namespace memopt
 {
-    typedef struct byte_t
+    typedef struct nibble_t
     {
         unsigned char data;
 
+        /// @brief Get highest nibble in data byte
+        /// @return A nibble transformed in unsigned char (1 byte)
         unsigned char getHighestNibble()
         {
             return ((data & 0b11110000) >> 0b100);
         }
 
+        /// @brief Get lowest nibble in data byte
+        /// @return A nibble transformed in unsigned char (1 byte)
         unsigned char getLowestNibble()
         {
             return (data & 0b00001111);
         }
 
+        /// @brief Set the highest nibble data
+        /// @param nibble An unsigned char (1 byte data) that only will be considered the first nibble (0xFF)
         void setHighestNibble(unsigned char nibble)
         {
             nibble = (nibble & 0b00001111);		// Make sure that only the values on range 0-15 are available
@@ -40,6 +46,8 @@ namespace memopt
             data = (data | nibble);
         }
 
+        /// @brief Set the lowest nibble data
+        /// @param nibble An unsigned char (1 byte data) that only will be considered the first nibble (0xFF)
         void setLowestNibble(unsigned char nibble)
         {
             nibble = (nibble & 0b00001111);     // Make sure that only the values on range 0-15 are available
